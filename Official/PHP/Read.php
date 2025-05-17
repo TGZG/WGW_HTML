be4369cebd6e6214063034fd0e8de8961c30d07b
AX0201 <136287080+AX0201@users.noreply.github.com>
2025-05-17 11:29:18 +0800

    服务器更新

--- Official/PHP/Read.php
+++ Official/PHP/Read.php
@@ -1,13 +1,8 @@
 <?php
 header('Content-Type: text/plain; charset=utf-8');
-function safeFilename($input) {
-  // 移除路径字符，只保留字母数字和下划线
-  return preg_replace('/[^a-zA-Z0-9_\-.]/', '', basename($input));
-}
-
 if(isset($_GET['Title'])) {
   $requestedFile = $_GET['Title'];
-  $filePath = __DIR__."\..\MD\\{$requestedFile}.md"; // 文件存储在指定目录
+  $filePath = __DIR__."/../MD/{$requestedFile}.md"; // 文件存储在指定目录
 
   if(file_exists($filePath)) {
     readfile($filePath);
